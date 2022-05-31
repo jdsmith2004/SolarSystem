17673fee703fc99ee86b70c93ac130e6a63be808
Milton Chino <miltonac97@byui.edu>
2022-05-31 13:21:27 -0400

    Movement with acceleration

--- game.py
+++ game.py
@@ -1,5 +1,3 @@
-import tkinter as tk
-from tkinter import Scale, simpledialog
 import math
 import arcade
 from abc import ABC, abstractmethod
@@ -36,7 +34,19 @@ URANUS_RADIUS = 25559
 NEPTUNE_RADIUS = 24764
 
 SCALE = 0.003
-SHIP_THRUST = 3
+SHIP_THRUST = 4
+SPRITE_SCALING = 0.5
+SCREEN_TITLE = "Solar System"
+
+
+# Speed limit
+MAX_SPEED = 3.0
+
+# How fast we accelerate
+ACCELERATION_RATE = 0.1
+
+# How fast to slow down after we let off the key
+FRICTION = 0.02
 
 class Point:
     """
@@ -60,7 +70,7 @@ class Velocity:
         Basic stat initialization.
         By default, the coordinates do not change, so the coefficients are specified as 0.
         """
-        self.dx = 1
+        self.dx = 0
         self.dy = 0
 
 class FlyingObjects(ABC):
@@ -74,11 +84,11 @@ class FlyingObjects(ABC):
         self.velocity = Velocity()
         self.alive = True
         self.path = relpath(obj)
-        self.texture = arcade.load_texture(self.path)
-        self.width = self.texture.width*SCALE*scale
-        self.height = self.texture.height*SCALE*scale
-        self.angle = 0
-        self.radius = 0
+        self._texture = arcade.load_texture(self.path)
+        self._width = self._texture.width*SCALE*scale
+        self._height = self._texture.height*SCALE*scale
+        self._angle = 0
+        self._radius = 0
         # self.direction = 0
         # self.speed = 0
 
@@ -93,14 +103,15 @@ class FlyingObjects(ABC):
     def bounds(self):
         """Method to limit the movement of the ship"""
         # This part needs some adjustments
-        if  self.center.x > SCREEN_WIDTH:
-            self.center.x -= SCREEN_WIDTH           
-        elif self.center.y > SCREEN_HEIGHT:
-            self.center.y -= SCREEN_HEIGHT
-        elif self.center.x < 0:
-            self.center.x += SCREEN_WIDTH
-        elif self.center.y < 0:
-            self.center.y += SCREEN_HEIGHT
+        if  self.center.x >= (SCREEN_WIDTH -self.width/2):
+            self.velocity.dx = 0           
+        elif self.center.x < self.width/2:
+            self.velocity.dx = 0
+        
+        if self.center.y > (SCREEN_HEIGHT - self.height/2):
+            self.velocity.dy = 0
+        elif self.center.y < self.height/2:
+            self.velocity.dy = 0
 
     # def wrap(self):
     #     """ a base method for deleting flying objects when they leave the screen"""
@@ -117,44 +128,68 @@ class FlyingObjects(ABC):
      texture or image needed in the project"""
     @abstractmethod
     def draw(self):
-        arcade.draw_texture_rectangle(self.center.x,self.center.y, self.width, self.height, self.texture, self.angle, 255)
+        arcade.draw_texture_rectangle(self.center.x,self.center.y, self._width, self._height, self._texture, self._angle, 255)
 
 
-class Ship(FlyingObjects):
+class Ship(arcade.Sprite):
     """
     This class describes all the characteristics and capabilities of the ship.
     """
-    def __init__(self):
-        """
-        Basic stat initialization
-        """
-        super().__init__("images/ship_1.png",25)
-        # self.img = "" # -> the path to the image
-        self.ship_dist = EARTH_DIST # -> distance between Sun and ship
-        self.center = Point() # -> coordinates of the center
-        self.center.x = 350 # -> x-coordinate changed
-        self.center.y = 350 # -> y-coordinate changed
-        self.velocity = Velocity() # -> values for changing coordinates
-        self.velocity.dx = SHIP_THRUST/2
-    
-    def move_left(self):
-        if (self.velocity.dx >0):
-            self.velocity.dx *=-1
-        super().advance()
-        # self.angle += self.velocity.dx
-        # self.center.x -= self.velocity.dx
-    
-    def move_right(self):
-        if (self.velocity.dx<0):
-            self.velocity.dx *=-1 
-        super().advance()
-        # self.angle -= self.velocity.dx
-        # self.center.x += self.velocity.dx
+    # def __init__(self):
+    #     """
+    #     Basic stat initialization
+    #     """
+    #     super().__init__("images/ship_1.png",25)
 
-    def draw(self): 
-        """ Call the abstract method"""
-        # arcade.draw_texture_rectangle(self.center.x,self.center.y, self.width, self.height, self.texture, self.angle, 255)
-        super().draw()
+    #     self.img = "" # -> the path to the image
+    #     self.ship_dist = EARTH_DIST # -> distance between Sun and ship
+    #     self.center = Point() # -> coordinates of the center
+    #     self.center.x = 350 # -> x-coordinate changed
+    #     self.center.y = 350 # -> y-coordinate changed
+    #     self.velocity = Velocity() # -> values for changing coordinates
+    #     self.velocity.dx = SHIP_THRUST/2
+    
+    # def move_left(self):
+    #     if (self.velocity.dx >0):
+    #         self.velocity.dx *=-1
+    #     else:
+    #         self.velocity.dx = SHIP_THRUST
+    #     super().advance()
+    #     # self.angle += self.velocity.dx
+    #     # self.center.x -= self.velocity.dx
+    
+    # def move_right(self):
+    #     if (self.velocity.dx<0):
+    #         self.velocity.dx *=-1
+    #     else:
+    #         self.velocity.dx = -SHIP_THRUST
+    #     super().advance()
+    #     # self.angle -= self.velocity.dx
+    #     # self.center.x += self.velocity.dx
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        # Check to see if we hit the screen edge
+        if self.left < 0:
+            self.left = 0
+            self.change_x = 0  # Zero x speed
+        elif self.right > SCREEN_WIDTH - 1:
+            self.right = SCREEN_WIDTH - 1
+            self.change_x = 0
+
+        if self.bottom < 0:
+            self.bottom = 0
+            self.change_y = 0
+        elif self.top > SCREEN_HEIGHT - 1:
+            self.top = SCREEN_HEIGHT - 1
+            self.change_y = 0
+
+    # def draw(self): 
+    #     """ Call the abstract method"""
+    #     # arcade.draw_texture_rectangle(self.center.x,self.center.y, self.width, self.height, self.texture, self.angle, 255)
+    #     super().draw()
 
 # This could be a planet base class
 class Planet(FlyingObjects):
@@ -232,17 +267,28 @@ class Game(arcade.Window):
     This class will then call the appropriate functions of
     each of the above classes.
     """
-    def __init__(self, width, height):
+    def __init__(self, width, height, title):
         """
         Sets up the initial conditions of the game
         :param width: Screen width
         :param height: Screen height
         """
-        super().__init__(width, height)
+        super().__init__(width, height, title)
         arcade.set_background_color(arcade.color.SMOKY_BLACK)
 
         # Create each object
-        self.ship = Ship() # -> ship created
+  # Variables that will hold sprite lists
+        self.player_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+
+        # Track the current state of what key is pressed
+        self.left_pressed = False
+        self.right_pressed = False
+        self.up_pressed = False
+        self.down_pressed = False
+        
         # Remember: Planet(name, p_dist, distX, radius)
         # p_dist - planet distance from the Sun. Will be used fof info part
         # distX - position on the screen. Can be related to the actual distance from the Sun.
@@ -259,31 +305,92 @@ class Game(arcade.Window):
             Planet("Neptune", NEPTUNE_DIST, 950, 20),
             # Planet("Pluto", PLUTO_DIST, 950, 8)
         ]
-        self.info = Info(self.ship.ship_dist, self.planets) # -> info part created
 
         # Variable for movement of the ship
         self.held_keys = set()
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+
+        # Set up the player
+
+        self.player_sprite = Ship("images/ship_1_fixed.png",SPRITE_SCALING) # -> ship created
+        # self.p = Player(":resources:images/animated_characters/female_person/femalePerson_idle.png",
+        #                             SPRITE_SCALING)
+        self.player_sprite.center_x = 350
+        self.player_sprite.center_y = 350
+        self.player_list.append(self.player_sprite)
+
+        self.info = Info(EARTH_DIST, self.planets) # -> info part created
 
     def on_draw(self):
         """
         Called automatically by the arcade framework.
         Handles the responsibility of drawing all elements.
         """
+        self.clear()
         # Clear the screen to begin drawing
         arcade.start_render()
 
         # Draw each object
-        self.ship.draw() # -> the ship is drawn
+        self.player_list.draw() # -> the ship is drawn
         for planet in self.planets:
             planet.draw() # -> each planet is drawn
+        
+        # Display speed
+        arcade.draw_text(f"X Speed: {self.player_sprite.change_x:6.3f}", 10, 50, arcade.color.BLACK)
+        arcade.draw_text(f"Y Speed: {self.player_sprite.change_y:6.3f}", 10, 70, arcade.color.BLACK)
 
         self.info.draw() # -> draw the list with distances
 
-    def update (self, delta):
+    def on_update (self, delta):
         """Logic for movement and other future features"""
-        self.check_keys()
+        # self.check_keys()
+        # self.ship.advance()
 
-        self.ship.advance()
+        if self.player_sprite.change_x > FRICTION:
+            self.player_sprite.change_x -= FRICTION
+        elif self.player_sprite.change_x < -FRICTION:
+            self.player_sprite.change_x += FRICTION
+        else:
+            self.player_sprite.change_x = 0
+
+        if self.player_sprite.change_y > FRICTION:
+            self.player_sprite.change_y -= FRICTION
+        elif self.player_sprite.change_y < -FRICTION:
+            self.player_sprite.change_y += FRICTION
+        else:
+            self.player_sprite.change_y = 0
+
+        # Apply acceleration based on the keys pressed
+        if self.up_pressed and not self.down_pressed:
+            self.player_sprite.change_y += ACCELERATION_RATE
+        elif self.down_pressed and not self.up_pressed:
+            self.player_sprite.change_y += -ACCELERATION_RATE
+        
+        if self.left_pressed and not self.right_pressed:
+            self.player_sprite.change_x += -ACCELERATION_RATE
+        elif self.right_pressed and not self.left_pressed:
+            self.player_sprite.change_x += ACCELERATION_RATE
+
+
+        if self.player_sprite.change_x > MAX_SPEED:
+            self.player_sprite.change_x = MAX_SPEED
+        elif self.player_sprite.change_x < -MAX_SPEED:
+            self.player_sprite.change_x = -MAX_SPEED
+
+        if self.player_sprite.change_y > MAX_SPEED:
+            self.player_sprite.change_y = MAX_SPEED
+        elif self.player_sprite.change_y < -MAX_SPEED:
+            self.player_sprite.change_y = -MAX_SPEED
+
+        # Call update to move the sprite
+        # If using a physics engine, call update on it instead of the sprite
+        # list.
+        self.player_list.update()
 
     def check_keys(self):
         """This function checks for keys that are being held down."""
@@ -293,16 +400,39 @@ class Game(arcade.Window):
         if arcade.key.RIGHT in self.held_keys:
             self.ship.move_right()
     def on_key_press(self, key: int, modifiers: int):
-        self.held_keys.add(key)
+        # self.held_keys.add(key)
+        if key == arcade.key.UP:
+            self.up_pressed = True
+        elif key == arcade.key.DOWN:
+            self.down_pressed = True
+        elif key == arcade.key.LEFT:
+            self.left_pressed = True
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = True
     
     def on_key_release(self, key: int, modifiers: int):
         """
         Removes the current key from the set of held keys.
         """
-        if key in self.held_keys:
-            self.held_keys.remove(key)
+        # if key in self.held_keys:
+        #     self.held_keys.remove(key)
+        if key == arcade.key.UP:
+            self.up_pressed = False
+        elif key == arcade.key.DOWN:
+            self.down_pressed = False
+        elif key == arcade.key.LEFT:
+            self.left_pressed = False
+        elif key == arcade.key.RIGHT:
+            self.right_pressed = False
 
 
 # Creates the game and starts it going
-window = Game(SCREEN_WIDTH, SCREEN_HEIGHT)
-arcade.run()
+def main():
+    """ Main function """
+    window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()

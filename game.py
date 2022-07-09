8451a1f0fa318d605511fa5a4075d81988a98fab
Milton Chino <miltonac97@byui.edu>
2022-07-09 00:05:39 -0400

    Info box added Fullscreen resize allowed, press "F" key Documentation updated Bug on Jupiter radius display Planets radius and distances scaled to make them visible

--- game.py
+++ game.py
@@ -7,6 +7,7 @@ relpath = lambda p: os.path.normpath(os.path.join(os.path.dirname(__file__), p))
 # Screen parameters
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 650
+
 # How many pixels to keep as a minimum margin between the character
 # and the edge of the screen.
 VIEWPORT_MARGIN = 200
@@ -16,29 +17,28 @@ CAMERA_SPEED = 0.1
 SOLAR_SYSTEM = 178619362920.544
 # Distance form Sun (km)
 MERCURY_DIST = 57900000
-VENUS_DIST = 108200000
-EARTH_DIST =  149600000
-MARS_DIST = 227900000
+VENUS_DIST   = 108200000
+EARTH_DIST   = 149600000
+MARS_DIST    = 227900000
 JUPITER_DIST = 778600000
-SATURN_DIST = 1433500000
-URANUS_DIST = 2872500000
+SATURN_DIST  = 1433500000
+URANUS_DIST  = 2872500000
 NEPTUNE_DIST = 4495100000
-PLUTO_DIST = 5884500000
-
+PLUTO_DIST   = 5884500000
 # Radius of planets (km)
-SUN_RADIUS = 432690
+SUN_RADIUS     = 432690
 MERCURY_RADIUS = 2439.5
-VENUS_RADIUS = 6054
-EARTH_RADIUS = 6378
-MARS_RADIUS = 3396
+VENUS_RADIUS   = 6054
+EARTH_RADIUS   = 6378
+MARS_RADIUS    = 3396
 JUPITER_RADIUS = 71492
-SATURN_RADIUS = 60268
-URANUS_RADIUS = 25559
+SATURN_RADIUS  = 60268
+URANUS_RADIUS  = 25559
 NEPTUNE_RADIUS = 24764
+PLUTO_RADIUS   = 7444 #this is not real
 
-SOLAR_SYSTEM = 178619362920.544
-SCALE_PLANET = 0.003
-SCALE_SYSTEM = 0.00000001
+SCALE_PLANET = 0.007
+SCALE_SYSTEM = 0.000001
 
 #Sun to Mercury size(Sun radius) to distance(kil) ratio
 SUN_MERCURY_RATIO = SUN_RADIUS / 67368000
@@ -46,20 +46,20 @@ SUN_MERCURY_RATIO = SUN_RADIUS / 67368000
 # Distance Ratios to Earth
 
 # Size Ratios to Sun(Kilometers)
-SUN_SIZE = 600
-MERCURY_SIZE = 1 / 277 * SUN_SIZE
-VENUS_SIZE = 1 / 113 * SUN_SIZE
-EARTH_SIZE = 1 / 108 * SUN_SIZE
-MARS_SIZE = 1 / 208 * SUN_SIZE
-JUPITER_SIZE = 1 / 9.7 * SUN_SIZE
-SATURN_SIZE = 1 / 11.4 * SUN_SIZE
-URANUS_SIZE = 1 / 26.8 * SUN_SIZE
-NEPTUNE_SIZE = 1 / 27.7 * SUN_SIZE
-PLUTO_SIZE = 1 / 585 * SUN_SIZE
+SUN_SIZE = 200
+MERCURY_SIZE = (1 / 277)* SUN_SIZE
+VENUS_SIZE = (1 / 113) * SUN_SIZE
+EARTH_SIZE = (1 / 108) * SUN_SIZE
+MARS_SIZE = (1 / 208) * SUN_SIZE
+JUPITER_SIZE = (1 / 9.7) * SUN_SIZE
+SATURN_SIZE = (1 / 11.4) * SUN_SIZE
+URANUS_SIZE = (1 / 26.8) * SUN_SIZE
+NEPTUNE_SIZE = (1 / 27.7) * SUN_SIZE
+PLUTO_SIZE = (1 / 585) * SUN_SIZE
 
 MERCURY_D = SUN_SIZE / SUN_MERCURY_RATIO
 
-SCREEN_TITLE = "Solar System 1.2"
+SCREEN_TITLE = "Solar System 1.3"
 SHIP_TURN_AMOUNT = 3
 SHIP_SPEED = 3
 
@@ -146,6 +146,7 @@ class Planet:
         distX - position on the screen. Can be related to the actual distance from the Sun. Now it is form the head.
         radius - planet radius. Can be related to the real numbers. Now it is form the head.
         """
+
         self.name = name # -> planet name (can be used for img name)
         self.p_dist = p_dist # -> planet distance from the Sun
         self.radius = radius # -> radius of the planet to help with scaling
@@ -154,45 +155,12 @@ class Planet:
         self.width = self.texture.width*SCALE_PLANET*radius
         self.height = self.texture.height*SCALE_PLANET*radius
         self.center = Point() # -> coordinates of the center
-        self.center.x = distX # -> x-coordinate changed according to the planet distance
-        self.center.y = 200 # -> y-coordinate changed to the default
+        self.center.x = distX*SCALE_SYSTEM # -> x-coordinate changed according to the planet distance
+        self.center.y = SCREEN_HEIGHT//2 # -> y-coordinate changed to the default
+        # Planet location need to be fixed
 
     def draw(self):
         arcade.draw_texture_rectangle(self.center.x,self.center.y, self.width, self.height, self.texture, 0, 255)
-
-class Info():
-    """
-    Class for the list of distances between planets and ship
-    """
-    def __init__(self, ship_dist, planets):
-        """
-        Create a dictionary with text and y-position on the screen for each line (planet)
-        """
-        self.dist_text = {
-            'line': [],
-            'start_y': []
-        }
-        start_y = SCREEN_HEIGHT - 20 # -> start value for y-position for first line
-        for planet in planets:
-            self.dist_text['line'].append(planet.name+": "+str(abs(ship_dist - planet.p_dist))) # -> text for a line (Ex.: Mars: 78300000)
-            self.dist_text['start_y'].append(start_y) # -> y-position for line
-            start_y -= 20 # -> change y-position for next line
-        self.start_x = 10 # -> x-position the same for all lines
-
-    def draw(self):
-        """
-        Puts the current distances on the screen.
-        Draw line by line on the light background.
-        """
-        # Backgroud for info part
-        arcade.draw_rectangle_filled(100, SCREEN_HEIGHT-110
-                                    ,200, 220
-                                    ,arcade.color.BEIGE, 0,)
-        # Text
-        for i in range(len(self.dist_text['line'])):
-            arcade.draw_text(self.dist_text['line'][i], 
-                            start_x=self.start_x, start_y=self.dist_text['start_y'][i], 
-                            font_size=12, color=arcade.color.NAVY_BLUE)
             
 class Game(arcade.Window):
     """
@@ -200,33 +168,40 @@ class Game(arcade.Window):
     This class will then call the appropriate functions of
     each of the above classes.
     """
-    def __init__(self, width, height, title):
+    def __init__(self):
         """
         Sets up the initial conditions of the game
         :param width: Screen width
         :param height: Screen height
         """
-        super().__init__(width, height, title)
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE, fullscreen=False)
         # arcade.set_background_color(arcade.color.SMOKY_BLACK)
         
+        width, height = self.get_size()
+        self.set_viewport(0, width, 0, height)
+        arcade.set_background_color(arcade.color.SMOKY_BLACK)
+
         # self.background = arcade.load_texture(r"C:\Users\Joseph Raymant\Documents\School\2022 Spring\Applied Programming(CSE 310)\Team\SolarSystem\SimpleSpace.jpg")
-        self.background = arcade.load_texture(r"images\\background.png")
+        self.background = arcade.load_texture(relpath ("images\\background.png"))
+        
         self.held_keys = set()
         # Create each object
         self.ship = Ship()
         
+        # values have been changed for testing purposes, they need to be fixed yet
         self.planets = [ # -> planets created
-            Planet("Sun", 0, -500, SUN_SIZE),
-            Planet("Mercury", MERCURY_DIST, 150, MERCURY_SIZE),
-            Planet("Venus", VENUS_DIST, 250, VENUS_SIZE),
-            Planet("Earth", EARTH_DIST, 350, EARTH_SIZE),
-            Planet("Mars", MARS_DIST, 450, MARS_SIZE),
-            Planet("Jupiter", JUPITER_DIST, 620, JUPITER_SIZE),
-            Planet("Saturn", SATURN_DIST, 800, SATURN_SIZE),
-            Planet("Uranus", URANUS_DIST, 925, URANUS_SIZE),
-            Planet("Neptune", NEPTUNE_DIST, 1050, NEPTUNE_SIZE),
-            Planet("Pluto", PLUTO_DIST, 1150, PLUTO_SIZE)
+            Planet("Sun", 0, 0, SUN_SIZE),
+            Planet("Mercury", MERCURY_DIST, MERCURY_DIST, MERCURY_SIZE),
+            Planet("Venus", VENUS_DIST, VENUS_DIST, VENUS_SIZE),
+            Planet("Earth", EARTH_DIST, EARTH_DIST, EARTH_SIZE),
+            Planet("Mars", MARS_DIST, MARS_DIST, MARS_SIZE),
+            Planet("Jupiter", JUPITER_DIST, JUPITER_DIST, JUPITER_SIZE),# there is a bug on jupiter radius display on info box.
+            Planet("Saturn", SATURN_DIST, SATURN_DIST, SATURN_SIZE),
+            Planet("Uranus", URANUS_DIST, URANUS_DIST, URANUS_SIZE),
+            Planet("Neptune", NEPTUNE_DIST, NEPTUNE_DIST, NEPTUNE_SIZE),
+            Planet("Pluto", PLUTO_DIST, PLUTO_DIST, PLUTO_SIZE)
         ]
+
 
         # Used in scrolling
         # Set the viewport boundaries
@@ -237,7 +212,32 @@ class Game(arcade.Window):
         self.camera_sprites = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
         self.camera_gui = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-        self.info = Info(EARTH_DIST, self.planets) # -> info part created
+        # Get viewport sizes
+        left, screen_width, botton, screen_height = self.get_viewport()
+        #Set default values for info box at the top left of the screen
+        self.place = ''
+        self.radius = ''
+        self.distance_from_earth = 0
+        self.box_width = (2/6)*screen_width
+        self.box_height = (1/4)*screen_height
+        self.box_center_x = self.box_width//2
+        self.box_center_y = screen_height-(self.box_height//2)
+
+    def on_resize(self, width, height):
+        """ This method is automatically called when the window is resized. """
+
+        # Call the parent. Failing to do this will mess up the coordinates,
+        # and default to 0,0 at the center and the edges being -1 to 1.
+        super().on_resize(width, height)
+        self.camera_sprites.resize(int(width), int(height))
+        self.camera_gui.resize(int(width), int(height))
+        
+        #Set new value to resize info box
+        self.box_width = (2.1/6)*width
+        self.box_height = (1/4)*height
+        self.box_center_x = self.box_width//2
+        self.box_center_y = height-(self.box_height//2)
+
 
     def on_draw(self):
         """
@@ -248,13 +248,19 @@ class Game(arcade.Window):
         # Clear the screen to begin drawing
         arcade.start_render()
         
-        # self.background = relpath(f"/images/background.png") # -> the path to the image
-        arcade.draw_lrwh_rectangle_textured(0,0,SCREEN_WIDTH,SCREEN_HEIGHT, self.background)
-        # arcade.draw_texture_rectangle(590, 350,SCREEN_WIDTH, SCREEN_HEIGHT,self.background)
-        # arcade.draw_texture_rectangle(590, 350,
-        #                                     SCREEN_WIDTH, SCREEN_HEIGHT,
-        #                                     self.background)
-        
+        # Get viewport dimensions
+        left, screen_width, bottom, screen_height = self.get_viewport()
+
+        # Draw Background
+        arcade.draw_lrwh_rectangle_textured(0,0,screen_width,screen_height, self.background)
+
+        text_size = 18
+        # Draw text on the screen so the user has an idea of what is happening
+        arcade.draw_text("Press F to toggle between full screen and windowed mode, unstretched.",
+                         screen_width // 2, screen_height // 2 - 20,
+                         arcade.color.WHITE, text_size, anchor_x="center")
+
+
         # Select the camera we'll use to draw all our sprites
         self.camera_sprites.use()
 
@@ -264,14 +270,6 @@ class Game(arcade.Window):
         
         # self.player_list.draw() # -> the ship is drawn
         self.ship.draw()
-        # Display speed
-        # ----------------------------HERE we need relative values for position
-        arcade.draw_text(f"X position: {self.ship.center.x:6.3f}", 10, 20, arcade.color.WHITE)
-        arcade.draw_text(f"Y position: {self.ship.center.y:6.3f}", 10, 40, arcade.color.WHITE)
-        arcade.draw_text(f"X vel: {self.ship.velocity.dx:6.3f}", 10, 60, arcade.color.WHITE)
-        arcade.draw_text(f"Y vel: {self.ship.velocity.dy:6.3f}", 10, 80, arcade.color.WHITE)
-
-        self.info.draw() # -> draw the list with distances
 
         # Select the (unscrolled) camera for our GUI
         self.camera_gui.use()
@@ -281,19 +279,61 @@ class Game(arcade.Window):
         text = f"Scroll value: ({self.camera_sprites.position[0]:5.1f}, {self.camera_sprites.position[1]:5.1f})"
         arcade.draw_text(text, 10, 10, arcade.color.BLACK_BEAN, 20)
 
-        # Draw the box that we work to make sure the user stays inside of.
-        # This is just for illustration purposes. We'd want to remove this
-        # in our game.
+        # Draw backgroud for info box
+        arcade.draw_rectangle_filled(self.box_center_x, self.box_center_y, self.box_width, self.box_height, arcade.color.ALMOND)
+        
+        # Draw text for info box
+        text_1 = f"Current Planet: {self.place}"
+        arcade.draw_text(text_1, self.box_center_x*0.05,screen_height-self.box_height*0.2, arcade.color.BLACK_BEAN, 15)
+        text_2 = f"Distance from center: {self.distance_from_earth}[Km]"
+        arcade.draw_text(text_2, self.box_center_x*0.05,screen_height-self.box_height*0.4, arcade.color.BLACK_BEAN, 15)
+        text_3 = f"Radius: {self.radius}[Km]"
+        arcade.draw_text(text_3, self.box_center_x*0.05,screen_height-self.box_height*0.6, arcade.color.BLACK_BEAN, 15)     
 
-        # left_boundary = VIEWPORT_MARGIN
-        # right_boundary = self.width - VIEWPORT_MARGIN
-        # top_boundary = self.height - VIEWPORT_MARGIN
-        # bottom_boundary = VIEWPORT_MARGIN
-        # arcade.draw_lrtb_rectangle_outline(left_boundary, right_boundary, top_boundary, bottom_boundary,
-        #                                    arcade.color.RED, 2)
     
     def check_collision(self):
         # this would help to check if we get in to a planet
+        for planet in self.planets:
+        
+        # if self.ship.alive and asteroid.alive:
+            too_close = planet.radius + Ship().width//2
+
+            if (abs(planet.center.x - self.ship.center.x) < too_close and abs(planet.center.y - self.ship.center.y) < too_close):
+                self.place = planet.name
+                self.radius = planet.radius
+                self.distance_from_earth = planet.p_dist
+
+                if self.place == "Sun":
+                    self.distance_from_earth = 0
+                    self.radius = SUN_RADIUS
+                if self.place == "Mercury":   
+                    self.distance_from_earth = MERCURY_DIST
+                    self.radius = MERCURY_RADIUS
+                if self.place == "Venus":   
+                    self.distance_from_earth = VENUS_DIST
+                    self.radius = VENUS_RADIUS
+                if self.place == "Earth":   
+                    self.distance_from_earth = EARTH_DIST
+                    self.radius = EARTH_RADIUS
+                if self.place == "Mars":   
+                    self.distance_from_earth = MARS_DIST
+                    self.radius = MARS_RADIUS
+                if self.place == "Jutiper":   
+                    self.distance_from_earth = JUPITER_DIST
+                    self.radius = JUPITER_RADIUS
+                if self.place == "Saturn":   
+                    self.distance_from_earth = SATURN_DIST
+                    self.radius = SATURN_RADIUS
+                if self.place == "Uranus":   
+                    self.distance_from_earth = URANUS_DIST
+                    self.radius = URANUS_RADIUS
+                if self.place == "Neptune":   
+                    self.distance_from_earth = NEPTUNE_DIST
+                    self.radius = NEPTUNE_RADIUS
+                if self.place == "Pluto":   
+                    self.distance_from_earth = PLUTO_DIST
+                    self.radius = PLUTO_RADIUS
+
         pass
 
     def check_keys(self):
@@ -317,6 +357,17 @@ class Game(arcade.Window):
 
     def on_key_press(self, key: int, modifiers: int):
         self.held_keys.add(key)
+
+        """Called whenever a key is pressed. """
+        if key == arcade.key.F:
+            # User hits f. Flip between full and not full screen.
+            self.set_fullscreen(not self.fullscreen)
+
+            # Get the window coordinates. Match viewport to window coordinates
+            # so there is a one-to-one mapping.
+            width, height = self.get_size()
+            self.set_viewport(0, width, 0, height)
+
       
     def on_key_release(self, key: int, modifiers: int):
         """
@@ -367,13 +418,11 @@ class Game(arcade.Window):
         # Scroll to the proper location
         position = self.view_left, self.view_bottom
         self.camera_sprites.move_to(position, CAMERA_SPEED)
-    
-    def on_resize(self, width, height):
-        """
-        Resize window
-        Handle the user grabbing the edge and resizing the window.
-        """
-        self.camera_sprites.resize(int(width), int(height))
-        self.camera_gui.resize(int(width), int(height))
-window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-arcade.run()
+
+def main():
+    Game()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
